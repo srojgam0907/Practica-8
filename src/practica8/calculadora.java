57985d1323203d46830c3b809db50a44d1437fa2
Oleglukyyy <oluk857@g.educaand.es>
2025-12-05 14:30:43 +0100

    Versión 1 division y sumas de OLEH. <En la luuuuuna>

--- src/practica8/calculadora.java
+++ src/practica8/calculadora.java
@@ -6,13 +6,24 @@ public class calculadora {
 		new calculadora().show();
 
 	}
-	
+
 	public void show() {
-		//TODO hacer funciones
-		int num1= pedirNumero();
-		int num2= pedirNumero();
-		int opcion= elegirOpcion();
-		
+		// TODO hacer funciones
+		int num1 = pedirNumero();
+		int num2 = pedirNumero();
+		int opcion = elegirOpcion();
+
 	}
 
+	public int dividir(int n1, int n2) {
+		if (n2 == 0) {
+			return 0;
+		}
+		return n1 / n2;
+
+	}
+
+	public int multiplicar(int n1, int n2) {
+		return n1 * n2;
+	}
 }
