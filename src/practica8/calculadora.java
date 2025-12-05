2f9c87ab6a4661ad64cb93ac5accff18ad1cfc6c
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-05 14:19:46 +0100

    Version 1

---
+++ src/practica8/calculadora.java
@@ -0,0 +1,18 @@
+package practica8;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		new calculadora().show();
+
+	}
+	
+	public void show() {
+		//TODO hacer funciones
+		int num1= pedirNumero();
+		int num2= pedirNumero();
+		int opcion= elegirOpcion();
+		
+	}
+
+}
